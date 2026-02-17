8d76caf8e5de1acc6735c2d1666fec8734be06fc
parmentier.g <gerome.parmentier@gmail.com>
2026-02-17 11:43:52 +0100

    global exception handler + test

--- src/test/java/fr/gerome/cleanarchitecture/infrastructure/restendpoints/GlobalExceptionHandlerTest.java
+++ src/test/java/fr/gerome/cleanarchitecture/infrastructure/restendpoints/GlobalExceptionHandlerTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doThrow;
 
 @WebMvcTest(AuthorController.class)
@@ -23,17 +28,13 @@ class GlobalExceptionHandlerTest {
     @Test
     void greetingShouldReturnMessageFromService() {
         doThrow(new IllegalArgumentException("argument invalid")).when(createAnAuthor).execute();
+        ProblemDetail expected = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "argument invalid");
+        expected.setInstance(URI.create("/authors"));
+
         restTestClient.post().uri("/authors").exchange()
-                .expectBody(String.class)
-                .isEqualTo("""
-                        {
-                          "detail": "argument invalid",
-                          "instance": "/authors",
-                          "status": 500,
-                          "title": "Internal Server Error"
-                        }""".replace("\n","")
-                        .replace(": ",":")
-                        .replace("  ",""));
+                .expectStatus().is5xxServerError()
+                .expectBody(ProblemDetail.class)
+                .consumeWith( response -> assertThat(response.getResponseBody()).isEqualTo(expected));
     }
 
 }
