941d02fb69c39ba9f927aa25cd51e2c0b084f70c
parmentier.g <gerome.parmentier@gmail.com>
2026-02-17 10:35:08 +0100

    initialization with Spring Initializr

---
+++ src/test/java/fr/gerome/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gerome.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
